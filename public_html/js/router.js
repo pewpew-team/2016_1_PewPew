3011cdd807a32e85a0bb1a4ff5770fec8d9ccc14
AlexHatesUnicorns <alex@MacBook-Air-gabriel.local>
2016-02-19 23:07:49 +0300

    listen to start game

--- public_html/js/router.js
+++ public_html/js/router.js
@@ -7,12 +7,12 @@ define(
                 'login': 'displayView',
                 'register': 'displayView',
                 'scoreboard': 'displayView',
-                'game': 'displayView',
                 '*default': 'defaultAction'
             },
             initialize: function () {
                 this.currentView = require('views/main');
                 this.listenTo(require('event'), 'navigate', this.changeRoute);
+                this.listenTo(require('event'), 'startGame', this.startGame)
             },
             displayView: function () {
                 var fragmentName = Backbone.history.getFragment();
@@ -28,6 +28,12 @@ define(
             },
             changeRoute: function (route) {
                 this.navigate(route, {trigger: true});
+            },
+            startGame: function() {
+                var view = require('views/game');
+                this.currentView.hide();
+                view.show();
+                this.currentView = view;
             }
         });
 
