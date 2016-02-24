c955065d9fabe65a33f0beddfa5dd88c0aa71780
AlexHatesUnicorns <alex@MacBook-Air-gabriel.local>
2016-02-24 12:13:05 +0300

    Scoreboard model
    Теперь рейтинг передается во вьюху из модели и далее в шаблон в виде
    json-a

--- public_html/js/views/scoreboard.js
+++ public_html/js/views/scoreboard.js
@@ -1,10 +1,14 @@
 define(
-    ['views/baseView','tmpl/scoreboard'],
+    ['views/baseView','tmpl/scoreboard','models/scoreboard'],
     function (baseView, tmpl) {
+        var scoreboard = require('models/scoreboard');
         var View = baseView.extend({
-            template: tmpl
+            template: tmpl,
+            render: function () {
+                this.$el.html(this.template(this.model.getScores()));
+            }
         });
 
-        return new View();
+        return new View({model: scoreboard});
     }
 );
