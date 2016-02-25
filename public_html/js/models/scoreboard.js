377cd7559ff135e7ffeed45b29a22a11e7f6bfc3
AlexHatesUnicorns <alex@MacBook-Air-gabriel.local>
2016-02-25 19:23:15 +0300

    Scoreboard model

--- public_html/js/models/scoreboard.js
+++ public_html/js/models/scoreboard.js
@@ -1,21 +1,30 @@
-define(['backbone'],
+define(['backbone', 'underscore'],
     function() {
         var Backbone = require('backbone');
+        var _ = require('underscore');
         var ScoreboardModel = Backbone.Model.extend({
             getScores: function() {
-                // Пока нет сервера сделано так
-                return {scores: [
+                this.sync();
+                this.order();
+                return {scores: this.get('scores')};
+            },
+            sync : function () {
+                this.set('scores',
+                    [{name: 'Alex', score: 100},
                     {name: 'Alex', score: 100},
                     {name: 'Alex', score: 100},
                     {name: 'Alex', score: 100},
                     {name: 'Alex', score: 100},
                     {name: 'Alex', score: 100},
                     {name: 'Alex', score: 100},
-                    {name: 'Alex', score: 100},
-                    {name: 'Alex', score: 100}
-                ]}
-
+                    {name: 'Alex', score: 100}]);
+            },
+            order: function () {
+                var scores = this.get('scores');
+                scores = _.sortBy(scores, 'name');
+                this.set('scores', scores);
             }
         });
+        return new ScoreboardModel();
     }
 )
--- public_html/js/views/scoreboard.js
+++ public_html/js/views/scoreboard.js
@@ -1,13 +1,14 @@
 define(
-    ['views/baseView','tmpl/scoreboard'],
+    ['views/baseView','tmpl/scoreboard', 'models/scoreboard'],
     function (baseView, tmpl) {
+        var scoreboard = require('models/scoreboard');
         var View = baseView.extend({
             template: tmpl,
             render: function () {
-                this.$el.html(this.template(this.model.getScores));
+                this.$el.html(this.template(this.model.getScores()));
             }
         });
 
-        return new View();
+        return new View({model: scoreboard});
     }
 );
