d768dc38f32952331c5942b1397329795d9f747b
AlexHatesUnicorns <alex@MacBook-Air-gabriel.local>
2016-03-02 21:54:25 +0300

    Typo fixed

--- public_html/js/views/game.js
+++ public_html/js/views/game.js
@@ -1,8 +1,7 @@
 define(function (require) {
         var tmpl = require('tmpl/game'),
             game = require('game/main'),
-            baseView = require('views/baseView'),
-            session = require('session');
+            baseView = require('views/baseView');
 
         var View = baseView.extend({
             template: tmpl,
