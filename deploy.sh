2ed68f52d0d5ced2069619d926b06b756d6ce89d
AlexHatesUnicorns <alex@MacBook-Air-gabriel.local>
2016-03-05 13:54:45 +0300

    fancy rsync deploy

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,5 @@ grunt compile
 echo "Deploy to $1 started"
 
 cd ./dist
-scp ./js/build.min.js root@$1:~/frontend/js/build.min.js
-scp ./css/main.min.css root@$1:~/frontend/css/main.min.css
-scp ./index.html root@$1:~/frontend/index.html
-scp -r ../public_html/img root@$1:~/frontend/img
+rsync -a --exclude=".*" ./ root@$1:~/frontend
+rsync -a --exclude=".*" ../public_html/img root@$1:~/frontend
