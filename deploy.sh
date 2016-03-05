20b825af67c7aee95e621e113982588e80e8d759
AlexHatesUnicorns <alex@MacBook-Air-gabriel.local>
2016-03-05 13:40:52 +0300

    Img deploy

--- deploy.sh
+++ deploy.sh
@@ -15,3 +15,4 @@ cd ./dist
 scp ./js/build.min.js root@$1:~/frontend/js/build.min.js
 scp ./css/main.min.css root@$1:~/frontend/css/main.min.css
 scp ./index.html root@$1:~/frontend/index.html
+scp -r ../public_html/img root@$1:~/frontend/img
