c1e4b011f9080a4eae3b2ef587ed6cf5d32193d6
itwillwork <nurullin_edgar@mail.ru>
2016-03-28 00:11:48 +0300

    добавлена кнопка выхода

--- public_html/js/views/game.js
+++ public_html/js/views/game.js
@@ -11,6 +11,10 @@ define(function (require) {
                 this.model = model;
                 baseView.prototype.initialize.apply(this, arguments);
                 $(window).on("resize", _.bind(this.resizeGameArea, this));
+                //$("#exit").on("click", function(){console.log("exit")});
+                $('#dynamicLayer').on("click", function(){
+                  alert('Вы нажали на элемент "foo"');
+                });
             },
             show: function () {
                 baseView.prototype.show.apply(this, arguments);
