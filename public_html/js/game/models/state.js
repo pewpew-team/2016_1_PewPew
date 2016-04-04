43867799f9e2235102ba21436614ebbd0990b480
AlexHatesUnicorns <alex@MacBook-Air-gabriel.local>
2016-04-04 12:08:29 +0300

    state model skeet

---
+++ public_html/js/game/models/state.js
@@ -0,0 +1,21 @@
+define(function(require) {
+  var Backbone = require('backbone');
+
+  var GameState = Backbone.Model.extend({
+    sendWS: function() {
+      // TODO
+    },
+    sendAjax: function() {
+      // TODO
+    },
+    collectPlayerData: function() {
+      // TODO
+    },
+    collectBulletsData: function() {
+      // TODO
+    },
+    collectBarriersData: function() {
+      // TODO
+    }
+  });
+});
